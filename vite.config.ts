a92ae34f19b4ba43076d2ad37614563819f7b349
FrigoAlex <alejo012903@hotmail.com>
2023-04-08 19:36:18 -0500

    🔧Add enviroment variables in the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    EnvironmentPlugin([
+      "VITE_API_KEY",
+      "VITE_API_URI"
+    ])
+    ,react()],
+});
