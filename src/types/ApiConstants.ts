55518ed55f9480cb25cdba7426025b6ddd875ddb
FrigoAlex <alejo012903@hotmail.com>
2023-04-08 21:07:24 -0500

    ♻️ Refactor ApiConstants to work with dotenv

--- src/types/ApiConstants.ts
+++ src/types/ApiConstants.ts
@@ -1,4 +1,8 @@
-export enum ApiConstants {
-  KEYAUTH = import.meta.env.VITE_API_KEY,
-  APIURI = import.meta.env.VITE_API_URI,
-}
+import * as dotenv from "dotenv";
+
+if (process.env.NODE_ENV === "test") dotenv.config();
+
+export const ApiConstants = {
+  KEYAUTH: process.env.VITE_API_KEY as string,
+  APIURI: process.env.VITE_API_URI as string,
+};
